833d1964222cab5c99e599b9dacd9efb332375ac
Teodor <teodor.pripoae@gmail.com>
2016-12-06 03:49:58 +0200

    Add example client for sofa pbrpc echo server

---
+++ example/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/kuende/sofa-pbrpc-go/sofa"
+)
+
+var (
+	serverAddr string
+	message    string
+)
+
+func main() {
+	flag.StringVar(&serverAddr, "s", "localhost:12321", "server address")
+	flag.StringVar(&message, "m", "Hello from qinzuoyan01", "message to send")
+	flag.Parse()
+
+	transport, err := sofa.NewTCPTransport([]string{serverAddr}, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to initialize transport: %v\n", err)
+		os.Exit(1)
+	}
+
+	clientConn, err := sofa.NewClient(transport)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to initialize client: %v\n", err)
+		os.Exit(1)
+	}
+
+	client := NewEchoServerClient(clientConn)
+	response, err := client.Echo(context.Background(), &EchoRequest{Message: proto.String(message)})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to send rpc: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "Received response: %v\n", response)
+}
